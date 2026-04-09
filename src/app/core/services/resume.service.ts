1353aa761fbf874f00640f45fb145fe385d86b61
megsoft1 <megsoft1@megsoft.mdu>
2026-04-09 15:11:51 +0530

    removed log

--- src/app/core/services/resume.service.ts
+++ src/app/core/services/resume.service.ts
@@ -58,7 +58,6 @@ export class ResumeService {
         {
           if(payload.id === 0 || payload.id === undefined){
           this.tokenService.setResumeId(res.resume_id.toString());
-          console.log('Personal details saved, resume ID set to:', res.resume_id);
            }
         }
       ) 
